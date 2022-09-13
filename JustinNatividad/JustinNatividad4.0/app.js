1ac5d07dd89404cd0573d1864bf1bd7d82b01aa9
justinotime <jjnatividad1209@gmail.com>
2022-09-12 17:43:14 -0700

    5:43 9/12/2022

---
+++ JustinNatividad/JustinNatividad4.0/app.js
@@ -0,0 +1,29 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+
+function PageTransitions() {
+    // Button click active class
+    for(let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    // Sections Active Class
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            // remove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+        }
+    })
+}
+
+PageTransitions();
